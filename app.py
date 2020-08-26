d17c9fa7c0d436e0560c304a6eeb44dbb55f4cbb
eleanorthomas <eleanor.thomas@myself.com>
2020-08-25 20:52:15 -0500

    Update Venue model

--- app.py
+++ app.py
@@ -36,10 +36,12 @@ class Venue(db.Model):
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    url = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    seeking_talent = db.Column(db.Boolean)
+    seeking_talent_msg = db.Column(db.String)
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
